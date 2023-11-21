86e1ee614b18fdec81130da8aaa216d73b19410d
Platon Slynko <platonslynko@Platons-MacBook-Pro.local>
2023-11-20 23:41:52 -0500

    resolved branches

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,12 @@ from flask_cors import CORS
 
 import os
 import openai
+import re
 
-with open('api.key', 'r') as file:
+
+from serpapi import GoogleSearch
+
+with open('backend/api.key', 'r') as file:
     api_key = file.readline().strip()  # Read the first line
 
 openai.api_key = api_key
@@ -93,19 +97,134 @@ def prompt():
     return js, 200
 
 
+@app.route('/showtimes', methods=['POST'])
+def showtimes():
+    recieved_data = recieved_request.json
+    question = recieved_data['question']
+
+    resp = dict()
+
+    resp['time'] = time.time()
+
+    # filter out non-showtimes question
+    if not check_if_showtimes_needed(question):
+        return "not showtimes question"
+    
+    current_movies = get_list_current_showtimes()
+    
+    
+
+    prompt = "Can you take a look at this list of movies and tell me which of the movies best addresses this question: '{question}' Answer only with the names of the movies and nothing else. If none of the movies work, then asnwer with a blank \n" + str(current_movies)
+
+    # prompt = prompt_template + f"%^& {question} %^& "
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[{"role": "user", "content": prompt}]
+    )
+    #print(completion)
+    ans = completion["choices"][0]["message"]["content"]
+
+    movie_suggestions = ans
+
+    # movie_list = list()
+
+    # try:
+    #     raw_list = ans.split("<movie_start>")[1:]
+    #     for raw_movie in raw_list:
+    #         split_raw_mov_tmp = raw_movie.split("<movie_end>")[0]
+    #         split_raw_mov = split_raw_mov_tmp.split(" > ")
+    #         # TO-DO:
+    #         # sptrip spaces and newlines from each elem, just in case
+    #         d = {
+    #             "name":  split_raw_mov[0],
+    #             "year":  split_raw_mov[1],
+    #             "actors":  split_raw_mov[2],
+    #             "desc":  split_raw_mov[3],
+    #         }
+    #         movie_list.append(d)
+    # except Exception as error:
+    #     # TO-DO:
+    #     # cannot parse the selected form
+    #     # return appropriate error page
+    #     raise error
+        
+    # resp['movie_suggestions'] = movie_list
+
+    # js = json.dumps(resp, indent=4)
+
+    
+    return movie_suggestions, 200
+
+
+
+def get_list_current_showtimes(location="Ann Arbor, Michigan, United States"):
+    params = {
+    "q": "movie showtimes",
+    "location": location,
+    "hl": "en",
+    "gl": "us",
+    "api_key": "0e7dbfbcc1a20145c678bca44458f6a785f8815510167755a047443d8b3f88e9"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    raw_movies = results['knowledge_graph']['movies_playing']
+    short_showtimes = [{"name": m['name'], "details": m['extensions'] }  for m in raw_movies]
+    return short_showtimes
+
+
+# def get_current_showtimes(location="Ann Arbor"):
+
+#     params = {
+#     "q": "eternals theater",
+#     "location": "Austin, Texas, United States",
+#     "hl": "en",
+#     "gl": "us",
+#     "api_key": "0e7dbfbcc1a20145c678bca44458f6a785f8815510167755a047443d8b3f88e9"
+#     }
+
+#     search = GoogleSearch(params)
+#     results = search.get_dict()
+#     showtimes = results["showtimes"]
+
+
+
+def check_if_showtimes_needed(question):
+
+    prompt = "Can you check whether the question in quotations mentions something to do with theaters or movies in theaters? Answer only 'yes' or 'no' and nothing else.\n" + question
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[{"role": "user", "content": prompt}]
+    )
+    #print(completion)
+    ans = completion["choices"][0]["message"]["content"]
+    
+    #reg_ex to parse 'yes' or 'no'
+    return bool(re.search(r'yes(?!.*no)', ans.lower()))
+
 
 @app.route('/test', methods=['GET'])
 def test():
     # data = recieved_request.json
 
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", 
-    messages=[{"role": "user", "content": "Tell me about AGI in 5 bullet points"}]
-    )
-    #print(completion)
-    ans = completion["choices"][0]["message"]["content"]
 
-    return ans, 200
+    q = "Is there a new crime movie in the theaters?"
+
+    current_movie_list =  get_list_current_showtimes()
+
+    prompt = f"Can you take a look at this list of movies and tell me which of the movies best addresses this question: '{q}' Answer only with the names of the movies and nothing else.\n"+str(current_movie_list)
+
+
+    # completion = openai.ChatCompletion.create(
+    # model="gpt-3.5-turbo", 
+    # messages=[{"role": "user", "content": "Tell me about AGI in 5 bullet points"}]
+    # )
+    # #print(completion)
+    # ans = completion["choices"][0]["message"]["content"]
+
+    return resp, 200
 
 
 
